1413c5a7e161fe391b3250771f12d708df10323c
Ludovic FAVRE <ludovic.favre-artigues-prestataire@laposte.fr>
2017-09-15 11:41:42 +0200

    Ajout du router

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -4,9 +4,26 @@ namespace Framework;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use GuzzleHttp\Psr7\Response;
+use Framework\Router;
 
 class App
 {
+    private $modules = [];
+    private $router;
+
+    /**
+    * App constructopr
+    * @param string[] $modules List of modules
+    */
+
+    public function __construct(array $modules = [])
+    {
+        $this->router = new Router();
+        foreach ($modules as $module) {
+          $this->modules[] = new $module($this->router);
+        }
+    }
+
     public function run(ServerRequestInterface $request): ResponseInterface
     {
         $uri = $request->getUri()->getPath();
@@ -15,10 +32,26 @@ class App
                                     ->withStatus(301)
                                     ->withHeader('Location', substr($uri, 0, -1));
         }
-        if ($uri === '/blog') {
-            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        $route = $this->router->match($request);
+        if(is_null($route)){
+          return new Response(404, [], '<h1>Erreur 404</h1>');
         }
-        $response = new Response(404, [], '<h1>Erreur 404</h1>');
-        return $response;
+        $params = $route->getParams();
+        $request = array_reduce(array_keys($params), function ($request, $key) use ($params) {
+          return $request->withAttribute($key, $params[$key]);
+        }, $request);
+
+        $response = call_user_func_array($route->getCallback(), [$request]);
+        if(is_string($response)){
+          return new Response(200, [], $response);
+        }
+        elseif ($response instanceof ResponseInterface) {
+          return $response;
+
+        }
+        else{
+          throw new \Exception("The response is not a string or an instanceof ResponseInterface", 1);
+
+        }
     }
 }
--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -3,7 +3,11 @@ namespace Tests\Framework;
 
 use Framework\App;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 use GuzzleHttp\Psr7\ServerRequest;
+use App\Blog\BlogModule;
+use Tests\Framework\Modules\ErroredModule;
+use Tests\Framework\Modules\StringModule;
 /**
  *
  */
@@ -12,7 +16,7 @@ class AppTest extends TestCase
 
   public function testRedirectTrainingSlash()
   {
-    $app = new App();
+    $app = new App([]);
     $request = new ServerRequest('GET','/demoSlash/');
     $response = $app->run($request);
     $this->assertContains('/demoSlash', $response->getHeader('Location'));
@@ -21,16 +25,43 @@ class AppTest extends TestCase
 
   public function testBlog()
   {
-    $app = new App();
+    $app = new App([
+      BlogModule::class
+    ]);
     $request = new ServerRequest('GET','/blog');
     $response = $app->run($request);
     $this->assertContains('<h1>Bienvenue sur le blog</h1>', (string) $response->getBody());
     $this->assertEquals('200', $response->getStatusCode());
+
+    $requestSingle = new ServerRequest('GET','/blog/article-de-test');
+    $responseSingle = $app->run($requestSingle);
+    $this->assertContains('<h1>Bienvenue sur l\'article :article-de-test</h1>', (string) $responseSingle->getBody());
+  }
+
+  public function testThrowExecptionIfNotResponseSend()
+  {
+    $app = new App([
+      ErroredModule::class
+    ]);
+    $request = new ServerRequest('GET','/demo');
+    $this->expectException(\Exception::class);
+    $app->run($request);
+  }
+
+  public function testConvertStringToResponseResponseSend()
+  {
+    $app = new App([
+      StringModule::class
+    ]);
+    $request = new ServerRequest('GET','/demo');
+    $response = $app->run($request);
+    $this->assertInstanceOf(ResponseInterface::class, $response);
+    $this->assertContains('DEMO', (string) $response->getBody());
   }
 
   public function testError4040()
   {
-    $app = new App();
+    $app = new App([]);
     $request = new ServerRequest('GET','/ioho');
     $response = $app->run($request);
     $this->assertContains('<h1>Erreur 404</h1>', (string) $response->getBody());
